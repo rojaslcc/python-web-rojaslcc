ff03d0a14adadf32262c88049675612062f5614b
Humberto Rojas <rojaslcc@msn.com>
2026-03-11 01:36:44 -0600

    Remover conflicto
    Remover conflicto

--- link_bio/build.sh
+++ link_bio/build.sh
@@ -6,11 +6,6 @@ source venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# Ejecuta el script para actualizar los seguidores
-echo "--- Ejecutando script para actualizar seguidores ---"
-python update_followers.py
-echo "--- Script de seguidores finalizado ---"
-
 rm -rf .web
 rm -f frontend.zip
 reflex export --frontend-only
