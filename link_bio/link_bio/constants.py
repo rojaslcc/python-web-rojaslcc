b51d7a48896ebe6065691bd0ae1cdf3598ea482b
Humberto Rojas <rojaslcc@msn.com>
2026-04-05 12:36:32 -0600

    video de la semana
    video de la semana

--- link_bio/link_bio/constants.py
+++ link_bio/link_bio/constants.py
@@ -8,7 +8,7 @@ LINKEDIN_URL = "https://linkedin.com/in/rojaslcc"
 
 # Lo nuevo
 ALTOSINTERACTIVEWEB_URL = "https://altosinteractive.com"
-YOUTUBE_BTSTH_PDOCASTDELASEMANA_URL = "https://www.youtube.com/watch?v=Seo8DWqWxVU"
+YOUTUBE_BTSTH_PDOCASTDELASEMANA_URL = "https://www.youtube.com/watch?v=aqXB1hRXPCE"
 
 # Comunidad
 TWITCH_URL = "https://twitch.tv/rojaslcc"
